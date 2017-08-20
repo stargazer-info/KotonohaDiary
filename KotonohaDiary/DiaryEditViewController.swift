f0c63f11b86a9731bec32bd5729209f20472355f
Nobuyuki Yamaguchi <nyama195@gmail.com>
2017-08-20 09:43:12 +0900

    日本語リソースを追加

--- KotonohaDiary/DiaryEditViewController.swift
+++ KotonohaDiary/DiaryEditViewController.swift
@@ -26,6 +26,11 @@ class DiaryEditViewController: UIViewController, NSFetchedResultsControllerDeleg
         }
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        textView.becomeFirstResponder()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
